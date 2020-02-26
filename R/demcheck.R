00de45361724766448fdb4045c85dcc8ea7681eb
John Bryant <john.r.bryant@gmail.com>
2020-02-26 18:50:10 +1300

    added demcheck.R

---
+++ R/demcheck.R
@@ -0,0 +1,12 @@
+
+#' demcheck: Checking functions for the dem packages
+#'
+#' Functions for checking inputs and data structures
+#' in the \code{dem} packages, eg \pkg{demarray}.
+#' End users would not normally call these functions directly.
+#' 
+#'
+#' @docType package
+#' @name demcheck
+NULL
+
